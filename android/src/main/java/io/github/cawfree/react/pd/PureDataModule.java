a0529c84f0d8f8322eeeb574eb5ae76d476e010a
cawfree <hello@cawfree.com>
2020-07-09 22:36:46 +0100

    chore: basic interface definitions

--- android/src/main/java/io/github/cawfree/react/pd/PureDataModule.java
+++ android/src/main/java/io/github/cawfree/react/pd/PureDataModule.java
@@ -4,24 +4,50 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableNativeArray;
+import com.facebook.react.bridge.ReadableNativeMap;
+import com.facebook.react.bridge.Arguments;
 
 public class PureDataModule extends ReactContextBaseJavaModule {
 
-    private final ReactApplicationContext reactContext;
+  /** Default Constructor. */
+  public PureDataModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
 
-    public PureDataModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-    }
+  @Override
+  public String getName() {
+    return "PureData";
+  }
 
-    @Override
-    public String getName() {
-        return "PureData";
-    }
+  @ReactMethod
+  public final void registerAudioController(final String pAudioControllerId, final ReadableMap pAudioProps, final Promise pPromise) {
+    pPromise.resolve(Arguments.createMap());
+  }
 
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
-    }
+  @ReactMethod
+  public final void unregisterAudioController(final String pAudioControllerId, final Promise pPromise) {
+    pPromise.resolve(Arguments.createMap());
+  }
+
+  @ReactMethod
+  public final void registerPatch(final String pAudioControllerId, final String pPatchId, final String pPatchData, final Promise pPromise) {
+    pPromise.resolve(Arguments.createMap());
+  }
+
+  @ReactMethod
+  public final void unregisterPatch(final String pAudioControllerId, final String pPatchId, final Promise pPromise) {
+    pPromise.resolve(Arguments.createMap());
+  }
+
+  @ReactMethod
+  public final void registerReceivers(final String pAudioControllerId, final String pPatchId, final ReadableMap pReceivers, final Promise pPromise) {
+    pPromise.resolve(Arguments.createMap());
+  }
+
 }
