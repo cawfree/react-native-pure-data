493994ff4cb0cc574f97c2f1ed5f4c1ce98fd503
cawfree <hello@cawfree.com>
2020-07-11 11:59:49 +0100

    chore: base invocation, requires sdk side-by-side

--- android/src/main/java/io/github/cawfree/react/pd/PureDataModule.java
+++ android/src/main/java/io/github/cawfree/react/pd/PureDataModule.java
@@ -13,7 +13,15 @@ import com.facebook.react.bridge.ReadableNativeArray;
 import com.facebook.react.bridge.ReadableNativeMap;
 import com.facebook.react.bridge.Arguments;
 
+import android.util.Log;
+
+import org.puredata.android.io.PdAudio;
+
 public class PureDataModule extends ReactContextBaseJavaModule {
+
+  private static final void debug(final String pString) {
+    Log.d("react-native-pure-data", pString);
+  }
 
   /** Default Constructor. */
   public PureDataModule(ReactApplicationContext reactContext) {
@@ -27,7 +35,34 @@ public class PureDataModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public final void registerAudioController(final String pAudioControllerId, final ReadableMap pAudioProps, final Promise pPromise) {
-    pPromise.resolve(Arguments.createMap());
+    debug("got here");
+    try {
+      final int sampleRate = pAudioProps.getInt("sampleRate");
+      final int numberOfChannels = pAudioProps.getInt("numberOfChannels");
+      final boolean inputEnabled = pAudioProps.getBoolean("inputEnabled");
+      final boolean mixingEnabled = pAudioProps.getBoolean("mixingEnabled");
+
+      debug("samplerate "+sampleRate);
+      debug("numberOfChannels "+numberOfChannels);
+      debug("inputEnabled "+inputEnabled);
+      debug("mixingEnabled "+mixingEnabled);
+
+      // XXX: Initialize the audio. I guess this means we can't have multiple
+      //      competing instances...
+      PdAudio.initAudio(
+          sampleRate,
+          numberOfChannels, // ?
+          numberOfChannels, // ?
+          8, // ?
+          true
+      );
+
+      debug("did init audio");
+      pPromise.resolve(Arguments.createMap());
+    } catch (final Exception pException) {
+      pException.printStackTrace();
+      pPromise.reject(pException);
+    }
   }
 
   @ReactMethod
